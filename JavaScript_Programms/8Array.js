5bff4380cdf7ac2df54b81ff0a222b09b56cd757
prathamesh nale <prathameshn7077@gmail.com>
2024-04-10 18:00:13 +0530

    add new topic

---
+++ JavaScript_Programms/8Array.js
@@ -0,0 +1,16 @@
+let arr=[];   //array litral
+
+let color=["red","blue","black"];
+console.log(color);
+console.log(color[0]);
+
+color[2]="voilet";   //size of array dynamic i.e Dynamic typed array.
+
+console.log(color);
+
+color[3]=5;    //we can store any data type i.e Dynamic typed array.
+
+console.log(color);
+
+//type of array in js is object
+//it is dynamic typed array (i.e both size and data can be change)
--- JavaScript_Programms/practice.js
+++ JavaScript_Programms/practice.js
@@ -1,11 +1,24 @@
-function newObj(value){
-    this.value=value,
-    this.draw=function(){
-        console.log("inside draw function of constructor function");
+function Person(name,email){
+    this.name=name;
+    this.email=email;
+    let id=5;
+    this.display=function(){
+        console.log("inside display function of Person");
     }
+
+    Object.defineProperty(this,"id",{
+        set:function(id1){
+            id=id1;
+        },
+        get:function(){
+            return id;
+        }
+    })
 }
 
 
-let obj1=new newObj(10);
-console.log(obj1.value);
-obj1.draw();
+let p1=new Person("prathamesh","prathameshn7077@gmail.com");
+console.log(p1.name,p1.email,p1.id);
+p1.display();
+p1.id=100;
+console.log(p1.id);
